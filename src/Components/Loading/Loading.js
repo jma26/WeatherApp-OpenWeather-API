e74737c98df76032386f6b5d0b278361a4e3ec19
Jesse Ma <j.masonoma17@gmail.com>
2021-10-08 17:13:36 -0700

    Add Loading component

---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,24 @@
+import './loading.css';
+
+const Loading = () => {
+  return (
+    <div className="Loading">
+      <div className="Loading__spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading;
